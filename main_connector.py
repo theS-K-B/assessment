290de2107691cf0fb1a033e6c7b11c1eb063b591
theS-K-B <mailskbat@gmail.com>
2021-10-18 01:35:45 +0530

    bug fixed

--- main_connector.py
+++ main_connector.py
@@ -18,7 +18,7 @@ for country in country_list:
     if_exists='replace')
 
 # get the data from specified country table
-def get_datafile(country="all"):
+def get_datafile(country):
     country = country.lower()
     df = pd.read_sql_table('table_'+country, engine, index_col='Customer_Id')
     # get data file as a csv

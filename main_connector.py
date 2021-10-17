7ab814e6e8f8892de468310ee0d4bfea7f908b37
theS-K-B <mailskbat@gmail.com>
2021-10-17 21:23:04 +0530

    codebase updated using sqlalchemy

--- main_connector.py
+++ main_connector.py
@@ -1,31 +1,7 @@
-# libraries needed
-import mysql.connector
 import pandas as pd
-from mysql.connector import errorcode
+import sqlalchemy as sa
 
-try:
-    cnx = mysql.connector.connect(
-        user     = 'root',
-        password = '12345',
-        host     = '127.0.0.1',
-        database = 'demo_db'
-    )
-except mysql.connector.Error as err:
-    if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
-        print("Something is wrong with your user name or password")
-    elif err.errno == errorcode.ER_BAD_DB_ERROR:
-        print("Database does not exist")
-    else:
-        print(err)
-
-# cursor = cnx.cursor()
-#query = 'select * from customers'
-
-# testing the cursor
-# cursor.execute(query)
-# table_rows = cursor.fetchall()
-
-# reading country specific data using Pandas
-df = pd.read_sql_query('select * from customers', con=cnx,  index_col='Customer_Id')
-print(df.loc[df['Country'] == 'IND'])
-
+# creating database connection obj
+engine = sa.create_engine('mysql+pymysql://root:12345@localhost:3306/demo_db')
+df = pd.read_sql_table('customers', engine, index_col='Customer_Id')
+print(df)

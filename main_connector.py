bbc0aadc7df7f3d7f4a213cd3a7b27045500b769
theS-K-B <mailskbat@gmail.com>
2021-10-18 01:02:43 +0530

    added code to retrieve country specific data in csv

--- main_connector.py
+++ main_connector.py
@@ -5,10 +5,23 @@ import sqlalchemy as sa
 engine = sa.create_engine('mysql+pymysql://root:12345@localhost:3306/demo_db')
 df = pd.read_sql_table('customers', engine, index_col='Customer_Id')
 
-# creating country based table
-df.loc[df['Country'] == 'IND'].to_sql(
-    name='india_table',
+# list of unique countries
+country_list = df['Country'].unique()
+
+# for each country create a diffrent table in database
+# e.g. table_ind, table_au
+for country in country_list:
+    df.loc[df['Country'] == country].to_sql(
+    name='table_'+country,
     con=engine,
     index=True,
-    if_exists='replace'
-)
+    if_exists='replace')
+
+# get the data from specified country table
+def get_datafile(country="all"):
+    country = country.lower()
+    df = pd.read_sql_table('table_'+country, engine, index_col='Customer_Id')
+    # get data file as a csv
+    df.to_csv('D:/projects/assessment/datafiles/' + country + ".csv")
+
+get_datafile("phil")

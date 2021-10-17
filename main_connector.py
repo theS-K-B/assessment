5a8569acd116f819cfec1f9c8eccd278d5d21703
theS-K-B <mailskbat@gmail.com>
2021-10-17 21:53:22 +0530

    country specific tables added to db

--- main_connector.py
+++ main_connector.py
@@ -4,4 +4,11 @@ import sqlalchemy as sa
 # creating database connection obj
 engine = sa.create_engine('mysql+pymysql://root:12345@localhost:3306/demo_db')
 df = pd.read_sql_table('customers', engine, index_col='Customer_Id')
-print(df)
+
+# creating country based table
+df.loc[df['Country'] == 'IND'].to_sql(
+    name='india_table',
+    con=engine,
+    index=True,
+    if_exists='replace'
+)
